013b2f8d9710ebc1a75a52d790e8af5a1c5699e4
vmetcalf <35386759+vmetcalf@users.noreply.github.com>
2018-01-19 12:18:27 -0500

    Added WTime and Period classes

---
+++ Period.java
@@ -0,0 +1,27 @@
+package com.example.vmetcalf.eop;
+
+/**
+ * Created by vmetcalf on 1/18/18.
+ */
+
+public class Period {
+    WTime start;
+    WTime end;
+    String period;
+    String className;
+    String room;
+    String teacher;
+
+    public Period(WTime start, WTime end, String Period){
+
+    }
+    public Period(WTime start, int durationMinutes, String Period) {
+
+    }
+    //getters and setters for the instance variables
+
+    // IsDuring(WTime now)
+
+    // ToString
+
+}
---
+++ WTime.java
@@ -0,0 +1,31 @@
+package com.example.vmetcalf.eop;
+
+/** Class to model a time of week
+ * Created by vmetcalf on 1/18/18.
+ */
+
+public class WTime {
+    int ticks;
+    //Lots of work to do here
+    public WTime(int day,int hour, int minute,int sec){
+
+    }
+    public WTime(int day,int hour, int minute){
+
+    }
+    public WTime(WTime t){
+
+    }
+    public WTime(){
+
+    }
+//Getters and setters for hours minutes seconds days
+    public int getHour(){
+        return 0;
+    }
+    // equals and compareTo
+    //  isBefore(WTime)
+    //toString method
+
+
+}
